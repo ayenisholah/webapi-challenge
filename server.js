7a74d8b602fba691058f27642212f2eb99517210
Shola Ayeni <ayenisholah@yahoo.com>
2019-07-12 09:59:31 +0100

    Endpoint for creating action

--- server.js
+++ server.js
@@ -8,6 +8,18 @@ const server = express();
 server.use(helmet());
 server.use(express.json());
 
+server.post('/', async (req, res) => {
+  try {
+    const action = await Actions.insert(req.body);
+    res.status(201).json(action);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      message: 'Error adding action'
+    });
+  }
+});
+
 server.get('/', async (req, res) => {
   try {
     const actions = await Actions.get();
